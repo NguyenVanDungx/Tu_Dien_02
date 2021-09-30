5320096989b682e327ec0e9137841c6e7900db6e
NguyenVanDungx <20020192@vnu.edu.vn>
2021-09-30 09:50:38 +0700

    Initial commit

---
+++ src/DictionaryCommandLine.java
@@ -0,0 +1,37 @@
+public class DictionaryCommandLine {
+
+    DictionaryManagement dicM = new DictionaryManagement();
+
+    /**
+     * getter, setter DicM
+     */
+    public DictionaryManagement getDicM() {
+        return dicM;
+    }
+
+    public void setDicM(DictionaryManagement dicM) {
+        this.dicM = dicM;
+    }
+
+    /**
+     * Ham in
+     */
+    public void showAllWords(Dictionary dic) {
+        //System.out.println(dic.getCount_word());
+        System.out.format("%-10s %-20s %-20s \n", "No", "| English", "| VietNamese");
+        for (int i = 0; i < dic.getCount_word(); i++) {
+            System.out.format("%-10s %-20s %-20s \n",
+                    i,
+                    dic.getWord(i).getWord_target(),
+                    dic.getWord(i).getWord_explain());
+        }
+    }
+
+    /**
+     * Hàm có chức năng gọi hàm insertFromCommandline() và showAllWords()
+     */
+    public void dictionaryBasic() {
+        dicM.insertFromCommandline();
+        showAllWords(dicM.getDic());
+    }
+}
